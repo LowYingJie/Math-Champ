3e3b3264161ef7ea6426b93aece339a2a75464e4
LowYingJie <lowyingjie02@gmail.com>
2025-01-12 05:05:36 +0800

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyB8dvbdto_TbLM13nShSb9uYuzOzHSJwe4",
+    authDomain: "math-champ-7d5a6.firebaseapp.com",
+    projectId: "math-champ-7d5a6",
+    storageBucket: "math-champ-7d5a6.firebasestorage.app",
+    messagingSenderId: "990106787184",
+    appId: "1:990106787184:web:f9d7c3d1b158083c1a03b5",
+    measurementId: "G-0BTLH51DC8"
+  };
+
+export default firebaseConfig;
